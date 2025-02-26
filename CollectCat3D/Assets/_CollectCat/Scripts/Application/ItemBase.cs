8fd231ac932561518bfc22dd648e42bbeca9b036
Hai3 <148192454+vothinuthao@users.noreply.github.com>
2025-02-26 15:19:41 +0700

    item mvc

---
+++ CollectCat3D/Assets/_CollectCat/Scripts/Application/ItemBase.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public abstract class ItemBase
+{
+    public int collectableQuantity;
+    
+    public string itemName;
+    public float speed;
+    public int itemQuantity;
+    
+    
+
+}
--- CollectCat3D/Assets/_CollectCat/Scripts/ItemSO.cs
+++ CollectCat3D/Assets/_CollectCat/Scripts/ItemSO.cs
@@ -5,8 +5,9 @@ public class ItemSO : ScriptableObject
     public string itemName;
     public int itemID;
     public GameObject item;
-    public int itemQuantity;
     public float speed;
     public Sprite iconItem;
+    public int itemQuantity;
+
 
 }
